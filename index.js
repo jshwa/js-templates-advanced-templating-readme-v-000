f515db0ae1bea83aae80f6d834d94b9298d2f6bd
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-17 23:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   for (var i=0; i<issues.length; i++){
     var result = template(issue[i]);
+    document.getElementsByTagName("main")[0].innerHTML += result;
   }
-  document.getElementsByTagName("main")[0].innerHTML += result;
 }
